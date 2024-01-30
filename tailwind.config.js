abfcf6e7e4a8dd4f44378a87e6f73117f855dfa1
Your Name <you@example.com>
2024-01-30 16:08:27 +0530

    cxsd

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,8 @@ module.exports = {
         'hero-pattern': "url('../src/images/home_banner_lg_bg.png')",
         'about_banner': "url('../src/images/about_banner.png')",
         'contact_bnr': "url('../src/images/contact_bnr.jpg')",
+        'termn': "url('../src/images/termn.jpg')",
+        'privacy': "url('../src/images/Privacy-.avif')",
       },
 
 
