708218b7c144aa1ffe34f27d4ab761b00bc15575
Your Name <you@example.com>
2024-01-31 17:21:22 +0530

    sdsd

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,17 +1,9 @@
-// toggle
-$(document).ready(function () {
-    $(".menu-btn a").click(function () {
-        $(".menu ").fadeToggle(200);
-        $(this).toggleClass('btn-open').toggleClass('btn-close');
-    });
-    $('.menu ').on('click', function () {
-        $(".menu ").fadeToggle(200);
-        $(".menu-btn a").toggleClass('btn-open').toggleClass('btn-close');
-    });
-    $('.menu a').on('click', function () {
-        $(".menu").fadeToggle(200);
-        $(".menu-btn a").toggleClass('btn-open').toggleClass('btn-close');
-    });
+$(".toggle").click(function () {
+    $(".navigation").css({ transform: "translateX(0)" });
+});
+
+$(".closebtn").click(function () {
+    $(".navigation").css({ transform: "translateX(-100%)" });
 });
 
 
